08c133fb89fb3526506c6a24451f0cb19e378a04
Rick Morales <rick.morales7693@gmail.com>
2023-07-31 12:07:10 -0500

    did more exercies in HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,138 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.print("Hello, World!\n");
-        System.out.print("Hello, World!\n");
-        System.out.print("Hello, World!\n");
-        System.out.print("Hello, World!\n");
+//        System.out.println("Hello, World!");
+//        System.out.print("Hello, World!\n");
+//        System.out.print("Hello, World!\n");
+//        System.out.print("Hello, World!\n");
+//        System.out.print("Hello, World!\n");
+//        System.out.println("Java is \"fun\"");
+//        System.out.println("/\\");
+//        String str = "hello";
+//        System.out.printf("I will say something here: %s", str);
+
+
+        //Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+//        int myFavoriteNumber = 7;
+//        System.out.println(myFavoriteNumber);
+
+
+        //Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+//        String myString = "This is a string value";
+
+//        System.out.println(myString);
+
+
+        //Change your code to assign a character value to myString. What do you notice?
+//        String myString = 'c';
+//        System.out.println(myString);
+
+        //Change your code to assign the value 3.14159 to myString. What happens? (Will work as a string not a number)
+//        String myString = "3.14159";
+//        System.out.println(myString);
+
+
+        //Declare a long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens? (Won't work because it is not initialized)
+//        long myNumber;
+//        System.out.println(myNumber);
+
+
+        //Change your code to assign the value 3.14 to myNumber. What do you notice? (Won't work because its not a whole number)
+//        long myNumber = 3.14;
+//        long myNumber = (long) 3.14;
+//        system.out.println(myNumber);
+
+
+        //Change your code to assign the value 123L (Note the 'L' at the end) to myNumber. (L is for long)
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+
+
+        //Change your code to assign the value 123 to myNumber.
+//        long myNumber = 123;
+//        System.out.println(myNumber);
+
+
+        //Change your code to declare myNumber as a float. Assign the value 3.14 to it.
+//        float myNumber = 3.14;
+//        float myNumber = (float) 3.14;
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+
+
+        //Copy and paste the following code blocks one at a time and execute them
+//        int x = 5;
+//        System.out.println(x++); // output: 5
+//        System.out.println(x); // output: 6
+
+//        int x = 5;
+//        System.out.println(++x); // output: 6
+//        System.out.println(x); // output: 6
+
+
+        //Try to create a variable named class. What happens? (Can't use reserved words)
+//        String class = "doesn't work";
+//        String class1 = "works";
+
+
+
+        //Object is the most generic type in Java. You can assign any value to a variable of type Object.
+//        String theNumberThree = "three";
+//        System.out.println(theNumberThree);
+//        Object o = theNumberThree;
+//        System.out.println(o);
+//        int three = (int) o; // error on this line because you can't cast a string to an int
+//        System.out.println(three);
+
+//        int three = (int) "three"; // error on this line because you can't cast a string to an int
+//        System.out.println(three);
+
+
+
+        //Rewrite the following expressions using the relevant shorthand assignment operators
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+////        x = x / y;
+//        x /= y;
+////        y = y - x;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
+
+
+        //What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold?
+        // What happens if you increment a numeric variable past the type's capacity?
+//        byte exampleByte = -200; // -128 to 127
+//        System.out.println(exampleByte);
+
+//        byte exampleByte = 127;
+////        exampleByte++;
+//        exampleByte += 1; // Will increment pas the types capacity, but will wrap around to the lowest value
+//        System.out.println(exampleByte);
+
+
+
+
+
+
+
+
+
+
+
 
     }
-
 
 }
