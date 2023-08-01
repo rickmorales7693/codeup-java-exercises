a29dbbeb84f19a8800a502dd6cf314ab816f56cf
Rick Morales <rick.morales7693@gmail.com>
2023-08-01 14:07:55 -0500

    finished ControlFlowExercises.java

--- src/exercises/ControlFlowExercises.java
+++ src/exercises/ControlFlowExercises.java
@@ -11,12 +11,10 @@ public class ControlFlowExercises {
 //        Loop Basics
 //
 //        // 1a. While
-//
 //        Create an integer variable i with a value of 5.
 //        Create a while loop that runs so long as i is less than or equal to 15
 //        Each loop iteration, output the current value of i, then increment i by one.
 //        Your output should look like this:
-//
 //        5 6 7 8 9 10 11 12 13 14 15
 
 //        int i = 5;
@@ -26,8 +24,8 @@ public class ControlFlowExercises {
 //        }
 
 
+
 //        // 1b. Do While
-//
 //        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
 //        Alter your loop to count backwards by 5's from 100 to -10.
 //        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
@@ -56,8 +54,8 @@ public class ControlFlowExercises {
 //        }while(i <= 1000000);
 
 
+
 //        // 1c. For
-//
 //        Refactor the previous two exercises to use a for loop instead.
 //        for(int i = 100; i >= -10; i -= 5){
 //            System.out.println(i);
@@ -85,32 +83,46 @@ public class ControlFlowExercises {
 
 
         // 3. Table of Powers
-//        System.out.println("\nEnter an integer: ");
-//        int userInt = sc.nextInt();
-//        System.out.println("\nHere is your table!\n");
-//        System.out.println("number | squared | cubed");
-//        System.out.println("------ | ------- | -----");
-//        for(int i = 1; i <= userInt; i++){
-//            System.out.printf("%-7d| %-8d| %-6d%n", i, i * i, i * i * i);
-//        }
-//        System.out.println("\nDo you want to continue? [y/n]");
-//        String userContinue = sc.next();
-//        if(userContinue.equalsIgnoreCase("y")){
-//            System.out.println("\nEnter an integer:");
-//            userInt = sc.nextInt();
+//        while (true) {
+//            System.out.print("\nEnter an number: ");
+//            int userInt = sc.nextInt();
 //            System.out.println("\nHere is your table!\n");
 //            System.out.println("number | squared | cubed");
 //            System.out.println("------ | ------- | -----");
-//            for(int i = 1; i <= userInt; i++){
+//            for (int i = 1; i <= userInt; i++) {
 //                System.out.printf("%-7d| %-8d| %-6d%n", i, i * i, i * i * i);
+//            }
+//            System.out.print("\nDo you want to continue? [y/n]: ");
+//            String userContinue = sc.next();
+//            if (!userContinue.equalsIgnoreCase("y")) {
+//                break;
 //            }
 //        }
 
 
 
-        // 4. Convert number grades into letter grades
-        System.out.println("\nEnter a number grade from 0 to 100");
-        int numberGrade = sc.nextInt();
+        // 4. Convert number grades into letter grades and edit the grade ranges to include pluses and minuses
+//        while(true){
+//            System.out.print("\nEnter a number grade from 0 to 100: ");
+//            int numberGrade = sc.nextInt();
+//            if(numberGrade >= 88){
+//                System.out.println("A");
+//            }else if(numberGrade >= 80){
+//                System.out.println("B");
+//            }else if(numberGrade >= 67){
+//                System.out.println("C");
+//            }else if(numberGrade >= 60){
+//                System.out.println("D");
+//            }else{
+//                System.out.println("F");
+//            }
+//            System.out.print("\nDo you want to continue? [y/n]: ");
+//            String userContinue = sc.next();
+//            if (!userContinue.equalsIgnoreCase("y")) {
+//                break;
+//            }
+//        }
+
 
 
 
