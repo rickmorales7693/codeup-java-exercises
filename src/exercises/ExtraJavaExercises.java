3eb645411b28e1601b4deb86ee8e21ad9c520cf0
Rick Morales <rick.morales7693@gmail.com>
2023-08-02 16:04:11 -0500

    added MethodsLecture.java

--- src/exercises/ExtraJavaExercises.java
+++ src/exercises/ExtraJavaExercises.java
@@ -8,17 +8,17 @@ public class ExtraJavaExercises {
 
 
         // 1. Write a program in Java to read 10 numbers from keyboard and find their sum and average.
-        int totalNumbers = 10;
-        int sum = 0;
-        System.out.println("Enter " + totalNumbers + " numbers ");
-        for (int i = 0; i < totalNumbers; i++) {
-            System.out.println("Enter number " + (i + 1) + " and press enter:");
-            int number = sc.nextInt();
-            sum += number;
-        }
-        double average = (double) sum / totalNumbers;
-        System.out.println("Sum: " + sum);
-        System.out.println("Average: " + average);
+//        int totalNumbers = 10;
+//        int sum = 0;
+//        System.out.println("Enter " + totalNumbers + " numbers ");
+//        for (int i = 0; i < totalNumbers; i++) {
+//            System.out.println("Enter number " + (i + 1) + " and press enter:");
+//            int number = sc.nextInt();
+//            sum += number;
+//        }
+//        double average = (double) sum / totalNumbers;
+//        System.out.println("Sum: " + sum);
+//        System.out.println("Average: " + average);
 
 
 
@@ -43,6 +43,124 @@ public class ExtraJavaExercises {
 
         // 5. Write a program in Java to make such a pattern like right angle triangle with number increased by 1.
 
+
+
+
+
+        /////Date Format Converter Program/////
+        // 6. Take on the following format: MM/DD/YYYY and Output the following: MonthName DD, YYYY (Example: input - 12/01/1999, output - December 12, 1999)
+
+//        System.out.println("Enter a date in the format MM/DD/YYYY:");
+//        String date = sc.nextLine();
+//        String month = date.substring(0, 2);
+//        String day = date.substring(3, 5);
+//        String year = date.substring(6);
+//        switch (month) {
+//            case "01":
+//                month = "January";
+//                break;
+//            case "02":
+//                month = "February";
+//                break;
+//            case "03":
+//                month = "March";
+//                break;
+//            case "04":
+//                month = "April";
+//                break;
+//            case "05":
+//                month = "May";
+//                break;
+//            case "06":
+//                month = "June";
+//                break;
+//            case "07":
+//                month = "July";
+//                break;
+//            case "08":
+//                month = "August";
+//                break;
+//            case "09":
+//                month = "September";
+//                break;
+//            case "10":
+//                month = "October";
+//                break;
+//            case "11":
+//                month = "November";
+//                break;
+//            case "12":
+//                month = "December";
+//                break;
+//        }
+//        System.out.println(month + " " + day + ", " + year);
+
+
+
+        //////Sentence Analyzer Program//////
+        // 7. Create a sentence analyzer program that tells the user how many vowels and consonants were used.
+        // Example: input - "The hill are alive."; output - 5 vowels and 10 consonants
+//        System.out.println("Enter a sentence:");
+//        String sentence = sc.nextLine();
+//        int vowels = 0;
+//        int consonants = 0;
+//        for (int i = 0; i < sentence.length(); i++) {
+//            char letter = sentence.charAt(i);
+//            if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u'
+//            || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U') {
+//                vowels++;
+//            } else if (letter != ' ') {
+//                consonants++;
+//            }
+//        }
+//        System.out.println(vowels + " vowels and " + consonants + " consonants");
+
+
+
+        //////Command Line Calculator//////
+        // 8. The program should welcome the user, prompt them for a calculation, and give results. The user should be prompted after
+        // each result is outputted if they want to enter another calculation. When they say no, give them an exit message. Allow
+        // the user first to specify how many operands they will enter. Do not worry about input validation (assume user enters
+        //a correct data type).
+//        System.out.println("Welcome to the Command Line Calculator!!!");
+//        System.out.println("Do you wish to enter a new calculation? (y/n)");
+//        String userResponse = sc.nextLine();
+//        while (userResponse.equalsIgnoreCase("y")) {
+//            System.out.println("How many operands will you calculated?");
+//            int numberOfOperands = sc.nextInt();
+//            sc.nextLine();
+//            System.out.println("Enter your first number:");
+//            double firstOperand = sc.nextDouble();
+//            sc.nextLine();
+//            double result = firstOperand;
+//            for (int i = 1; i < numberOfOperands; i++) {
+//                System.out.println("Enter your next number:");
+//                double nextOperand = sc.nextDouble();
+//                sc.nextLine();
+//                System.out.println("Enter your operator (+, -, *, /):");
+//                String operator = sc.nextLine();
+//                switch (operator) {
+//                    case "+":
+//                        result += nextOperand;
+//                        break;
+//                    case "-":
+//                        result -= nextOperand;
+//                        break;
+//                    case "*":
+//                        result *= nextOperand;
+//                        break;
+//                    case "/":
+//                        result /= nextOperand;
+//                        break;
+//                    default:
+//                        System.out.println("Invalid operator");
+//                        break;
+//                }
+//            }
+//            System.out.println("Result: " + result);
+//            System.out.println("Do you wish to enter a new calculation? (y/n)");
+//            userResponse = sc.nextLine();
+//        }
 
 
 
---
+++ src/lectures/MethodsLecture.java
@@ -0,0 +1,143 @@
+package lectures;
+
+public class MethodsLecture {
+
+    /*
+        METHOD STRUCTURE...
+
+        VISIBILITY_MODIFIER STATIC_OR_INSTANCE RETURN_TYPE METHOD_NAME(PARAM_DATATYPE PARAM, ...) {
+
+            BODY OF METHOD HERE
+
+        }
+
+        For Example...
+
+        public static void main(String[] args) {
+            System.out.println("Hello World");
+        }
+
+
+     */
+
+    // ----------- DEFINING AND CALLING METHODS WITH VARIOUS INPUTS (examples)
+
+    // no input or returned output
+
+//    public static void sayGreeting() {
+//        System.out.println("Hello");
+//    }
+
+    // no returned output with input
+
+//    public static void sayInput(String input) {
+//        System.out.println(input);
+//    }
+
+    // returned output without input
+
+//    public static int returnSeven() {
+//        return 7;
+//    }
+
+    // return output given an input
+
+//    public static int returnInt(int someInt) {
+//        return someInt;
+//    }
+
+    // multiple parameters
+
+//    public static double returnProductOfThreeNums(double num1, double num2, double num3) {
+//        return num1 * num2 * num3;
+//    }
+
+
+    // using a method inside another method
+
+//    public static int returnSumOfInts(int intOne, int intTwo) {
+//        return returnInt(intOne) + returnInt(intTwo);
+//    }
+
+
+    // return type different from input type
+
+//    public static int returnLengthOfString(String input) {
+//        return input.length();
+//    }
+
+    // multiple input types
+
+//    public static String repeatStr(String str, int times) {
+//        return str.repeat(times);
+//    }
+
+//    public static void printCharMultipleTimes(char c, int repetitions) {
+//        System.out.println(String.valueOf(c).repeat(repetitions));
+//    }
+
+
+    /**
+     * METHOD MINI-EXERCISES
+     */
+
+    // TODO: Write a method, returnName, that returns the string of your name.
+    // TODO: Write a method, returnLongerString, that takes in two strings and returns the longer of the two strings.
+    // TODO: Write a method, canBeHalved, that takes in an integer and returns true if the input integer is evenly divisible by two.
+    // TODO: !! Complete math exercises from the curriculum !!
+
+
+    // ----------- OVERLOADING
+
+    /*
+
+        Method overloading is a way to define the same method with different method signatures.
+        It is a convenience to prevent the programmer from having to create many more method names
+        when defining functionality for various data type inputs.
+
+        Much of the Java language is defined using overloaded methods (e.g. println())
+
+     */
+
+    // overloading example
+
+//    public static void sayHello() {
+//        System.out.println("Hello World");
+//    }
+//
+//    public static void sayHello(int num) {
+//        for (int i = 0; i < num; i += 1) {
+//            sayHello();
+//        }
+//    }
+
+    // Is this overloading???
+
+//        public static String sayHello(int num) {
+//            return "Hello World";
+//        }
+
+    // Is this overloading???
+
+//        public static String sayHello(String punctuation) {
+//            return "Hello World" + punctuation;
+//        }
+
+
+
+    /**
+     * OVERLOAD MINI-EXERCISES
+     */
+
+    // TODO: create a method, sayName, that can take in a single name String input or two name String inputs and will return
+    //  a greeting message to the user by either their first or first and last name.
+
+    // example: sayName("Justin") - "Hi Justin"
+    // example: sayName("Justin", "Reich") - "Hi Justin Reich"
+
+
+    // TODO: overload the math exercises from the curriculum exercise to work with both integers and doubles
+
+
+
+}
