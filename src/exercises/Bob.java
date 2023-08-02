53aa8b0364104d8f003773a67151149d1a117796
Rick Morales <rick.morales7693@gmail.com>
2023-08-02 11:18:31 -0500

    added Bob.java

---
+++ src/exercises/Bob.java
@@ -0,0 +1,21 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Say something to Bob: ");
+        String userInput = sc.nextLine();
+        if (userInput.endsWith("?")) {
+            System.out.println("Sure.");
+        } else if (userInput.endsWith("!")) {
+            System.out.println("Whoa, chill out!");
+        } else if (userInput.isEmpty()) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Whatever.");
+        }
+
+    }
+}
--- src/exercises/StringExercise.java
+++ src/exercises/StringExercise.java
@@ -6,25 +6,17 @@ public class StringExercise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+    String message1 = "We don't need no education\nWe don't need no thought control\n";
+    System.out.println(message1);
 
+    String message2 = "Check \"this\" out!, \"s inside of \"s!\n";
+    System.out.println(message2);
 
+    String message3 = "In windows, the main drive is usually C:\\\n";
+    System.out.println(message3);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    String message4 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!\n";
+    System.out.println(message4);
     }
 }
+
