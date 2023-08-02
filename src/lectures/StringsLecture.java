b342318f1f2b6f33c9f0a946fc57a37b0844962a
Rick Morales <rick.morales7693@gmail.com>
2023-08-02 15:13:42 -0500

    added StringsLecture.java

---
+++ src/lectures/StringsLecture.java
@@ -0,0 +1,140 @@
+package lectures;
+
+public class StringsLecture {
+
+    public static void main(String[] args) {
+
+//        int x = 2;
+//        int y = 2;
+//
+//        String str1 = "Bob";
+//        String str2 = "Bob";
+//        System.out.println(str1 == str2);
+//
+////        String str1 = "Bob";
+////        String str2 = str1;
+//
+//
+//        System.out.println(x == y);
+////         .equals(String anotherString)
+//
+//         String one = "1";
+//         String two = "2";
+//         System.out.println(one.equals(two));
+//
+//        if ("bob".equals("bob")) {
+//            System.out.println("Strings match!");
+//        }
+//
+//
+////         .equalsIgnoreCase(String anotherString)
+//
+//         String greeting = "howdy";
+//         System.out.println(greeting.equalsIgnoreCase("HoWDy"));
+//
+//
+////         .startsWith(String prefix)
+//
+//         String title = " Dr. Doctor";
+//         boolean isADoctor = title.toLowerCase()
+//                 .trim()
+//                 .startsWith("dr");
+//         System.out.println(isADoctor);
+//
+//
+////         .endsWith(String suffix)
+//
+//        String travisParkAddress = "311 E Travis St, San Antonio, TX 78205";
+//        String codeUpZip = "78205";
+//        boolean sameZipAsCodeup = travisParkAddress.trim().endsWith(codeUpZip);
+//        System.out.println(sameZipAsCodeup);
+//
+//
+////         .charAt(int index)
+////         consider that this method returns a char type and not a String type
+//
+//        boolean firstLetterCapitalized = false;
+//        String word = "test";
+//        char firstLetter = word.charAt(0);
+//
+//        if (firstLetter == Character.toUpperCase(firstLetter)) {
+//            firstLetterCapitalized = true;
+//        }
+//
+//        System.out.println(firstLetterCapitalized);
+//
+//
+////         .indexOf(String subString)
+//
+//         String cat = "cat";
+//         System.out.println(cat.indexOf("z"));
+//
+//
+////         .lastIndexOf(String subString)
+//
+//         String good = "Good";
+//         System.out.println(good.lastIndexOf("o"));
+//
+//
+////         .length()
+//
+//         String longestWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
+//         System.out.println(longestWord.length());
+//
+//
+////         .replace(char oldChar, char newChar)
+//
+//         String search = "tiny cats - cute cats";
+//         String parsedSearch = search.replace("cats", "dogs");
+//         System.out.println(parsedSearch);
+//
+//
+////         .substring(int beginIndex[, int endIndex])
+//
+//        String name = "Justin Bob Reich";
+//        String firstName = name.substring(0, name.indexOf(" "));
+//        System.out.println(firstName);
+//
+//        String lastName = name.substring(name.lastIndexOf(" ") + 1);
+//        System.out.println(firstName);
+//        System.out.println(lastName);
+//
+//        String name = "Justin Reich";
+//        String firstName = name.substring(0, name.indexOf(" "));
+//        System.out.println(firstName);
+//        String lastName = name.substring(firstName.length() + 1);
+//        System.out.println(lastName);
+//
+//
+////         .toLowerCase()
+////         .toUpperCase()
+//
+//        String hello = "hELlo";
+//        System.out.println(hello.toUpperCase());
+//        System.out.println(hello.toLowerCase());
+//        System.out.println(hello);
+//
+//
+////         .trim()
+//
+//        String input = "   bob      smith \n\n  ";
+//        String trimmedInput = input.trim();
+//        System.out.println(trimmedInput);
+//
+//
+////         convert word to pig latin word (assume word doesn't begin with a vowel or end in ay)
+//
+//        String word1 = "dog";
+//        String pigLatinWord = word1
+//            .trim() // "dog"
+//            .toLowerCase() // "dog"
+//            .substring(1) // "og"
+//            .concat(word1.substring(0, 1).toLowerCase()) // "og" + "d"
+//            .concat("ay"); // "ogday"
+//        System.out.println(pigLatinWord);
+//
+//        String output = "Hello".concat("World");
+//        System.out.println(output);
+
+    }
+}
