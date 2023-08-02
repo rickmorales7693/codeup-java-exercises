0bfb4638ca3f73a6e6f8c44d5f7f49e005323797
Rick Morales <rick.morales7693@gmail.com>
2023-08-02 15:07:50 -0500

    updated StringExercise.java

--- src/exercises/StringExercise.java
+++ src/exercises/StringExercise.java
@@ -1,22 +1,15 @@
 package exercises;
 
-import java.util.Scanner;
-
 public class StringExercise {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-    String message1 = "We don't need no education\nWe don't need no thought control\n";
-    System.out.println(message1);
+        System.out.println("We don't need no education\nWe don't need no thought control\n");
 
-    String message2 = "Check \"this\" out!, \"s inside of \"s!\n";
-    System.out.println(message2);
+    System.out.println("Check \"this\" out!, \"s inside of \"s!\n");
 
-    String message3 = "In windows, the main drive is usually C:\\\n";
-    System.out.println(message3);
+    System.out.println("In windows, the main drive is usually C:\\\n");
 
-    String message4 = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!\n";
-    System.out.println(message4);
+    System.out.println("I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!\n");
     }
 }
 
