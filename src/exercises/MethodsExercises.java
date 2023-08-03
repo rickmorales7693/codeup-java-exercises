68ebbaaefb4c6ffb2b8d6784162d5e6f51e4110c
Rick Morales <rick.morales7693@gmail.com>
2023-08-03 11:43:31 -0500

    added more solutions on MethodPractice and MethodsExercises

--- src/exercises/MethodsExercises.java
+++ src/exercises/MethodsExercises.java
@@ -4,20 +4,39 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-
-
-
-
-
-
-
+        System.out.println(addition(2, 2));
+        System.out.println(subtraction(2, 2));
+        System.out.println(multiplication(2, 2));
+        System.out.println(division(2, 2));
+        System.out.println(getInteger(1, 10));
 
     }
 
+    public static double addition(double num1, double num2){
+        return num1 + num2;
+    }
 
+    public static double subtraction(double num1, double num2){
+        return num1 - num2;
+    }
 
+    public static double multiplication(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double division(double num1, double num2){
+        return num1 / num2;
+    }
+
+    public static double getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+        if(userInput >= min && userInput <= max){
+            return userInput;
+        } else {
+            return getInteger(min, max);
+        }
+    }
 
 
 
---
+++ src/lectures/MethodPractice.java
@@ -0,0 +1,43 @@
+package lectures;
+
+public class MethodPractice {
+    public static void main(String[] args) {
+        System.out.println(returnName());
+
+        String longerString = returnLongerString("Rick", "Morales");
+        System.out.println(longerString);
+
+        System.out.println(canBeHalved(4));
+
+        System.out.println(sayName("Rick"));
+        System.out.println(sayName("Rick", "Morales"));
+    }
+
+    public static String returnName() {
+        return "Rick";
+    }
+
+    public static String returnLongerString(String str1, String str2){
+//        if (str1.length() > str2.length()) {
+//            return str1;
+//        } else {
+//            return str2;
+//        }
+        return (str1.length() >= str2.length()) ? str1 : str2;
+    }
+
+    public static boolean canBeHalved(int integer){
+        return integer % 2 == 0;
+    }
+
+    public static String sayName(String firstName){
+        return "Hi " + firstName;
+    }
+
+    public static String sayName(String firstName, String lastName){
+        return "Hi " + firstName + " " + lastName;
+    }
+
+
+
+}
