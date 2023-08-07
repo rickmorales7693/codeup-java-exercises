fd9ed8cadac4d66e062da853e04a3c202b5f7881
Rick Morales <rick.morales7693@gmail.com>
2023-08-07 15:09:43 -0500

    finishing touches to ArraysExercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,24 +4,26 @@ public class ArraysExercises {
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);
         System.out.println(Arrays.toString(numbers));
 
-        String[] person = {"Chuck", "Larry", "Moe"};
-        System.out.println(Arrays.toString(person));
-        System.out.println(person[0]);
-        System.out.println(person[1]);
-        System.out.println(person[2]);
+        Person[] people = {
+                new Person("Bob"),
+                new Person("Marly"),
+                new Person("Chuck"),
+        };
 
-        person = addPerson(person, "Curly");
-        System.out.println(Arrays.toString(person));
+        people = addPerson(people, new Person("Larry"));
+        for (Person person : people){
+            System.out.println(person.getName());
+        }
+
 
     }
 
-    public static String[] addPerson(String[] person, String personToAdd){
-        String[] newArray = Arrays.copyOf(person, person.length + 1);
-        newArray[newArray.length - 1] = personToAdd;
-        return newArray;
+    private static Person[] addPerson(Person[] people, Person personToAdd){
+        Person[] morePeople = Arrays.copyOf(people, people.length + 1);
+        morePeople[morePeople.length - 1] = personToAdd;
+        return morePeople;
     }
 
 }
