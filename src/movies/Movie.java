4fd38a7577e209ec6b8b5b9b97d01442271826b6
Rick Morales <rick.morales7693@gmail.com>
2023-08-07 19:41:34 -0500

    transfered function from MoviesApplication to Movie so now MA has a 2 liner

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -89,6 +89,64 @@ public class Movie {
         System.out.println("Goodbye!");
     }
 
+    public void manageMovies() {
+        Movie movie = new Movie();
+        boolean keepRunning = true;
+        while (keepRunning) {
+            displayMenu();
+            int userInput = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userInput){
+                case 0:
+                    movie.exit();
+                    break;
+                case 1:
+                    movie.printMovies();
+                    break;
+                case 2:
+                    movie.printMovies("animated");
+                    break;
+                case 3:
+                    movie.printMovies("drama");
+                    break;
+                case 4:
+                    movie.printMovies("horror");
+                    break;
+                case 5:
+                    movie.printMovies("scifi");
+                    break;
+                case 6:
+                    movie.addMovie();
+                    break;
+                case 7:
+                    movie.deleteMovie();
+                    break;
+                case 8:
+                    movie.editMovie();
+                    break;
+                case 9:
+                    movie.searchMovie();
+                    break;
+            }
+        }
+    }
+
+    private void displayMenu() {
+        System.out.println("What would you like to do?");
+        System.out.println("0 - exit");
+        System.out.println("1 - view all movies");
+        System.out.println("2 - view movies in the animated category");
+        System.out.println("3 - view movies in the drama category");
+        System.out.println("4 - view movies in the horror category");
+        System.out.println("5 - view movies in the sci-fi category");
+        System.out.println("6 - add a movie");
+        System.out.println("7 - delete a movie");
+        System.out.println("8 - edit a movie");
+        System.out.println("9 - search for a movie");
+        System.out.print("Enter your choice: ");
+    }
+
 
 
 }
