5c096f2d88bb033f9ac5c815617a339f4e60d6ef
Rick Morales <rick.morales7693@gmail.com>
2023-08-10 19:30:52 -0500

    just in case

--- src/ExceptionExercises.java
+++ src/ExceptionExercises.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 public class ExceptionExercises {
 
     public static void main(String[] args) {
-        divideNums(4, 0);
-        divideNums(1, 0);
-        divideNums(2, 0);
-        divideNums(5, 0);
+        System.out.println(divideNums(10, 2));
+        System.out.println(divideNums(10, 0));
         delayedHello(3);
 
         Scanner sc = new Scanner(System.in);
@@ -16,15 +14,12 @@ public class ExceptionExercises {
         displayTypingEffect(input);
     }
 
-    public static int divideNums(int num1, int num2) {
-        /// Cannot use IllegalArgumentException because it is a RuntimeException
-        try {
-            return num1 / num2;
-        } catch (ArithmeticException e) {
-            System.out.println("Cannot divide by 0");
-            return 0;
-        }
+    public static double divideNums(int num1, int num2) {
 
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by 0!");
+        }
+            return (double) num1 / num2;
     }
 
     public static void delayedHello(int seconds) {
@@ -36,10 +31,10 @@ public class ExceptionExercises {
         System.out.println("Hello!");
     }
 
-    public static void displayTypingEffect(String input){
+    public static void displayTypingEffect(String input) {
         Random rand = new Random();
 
-        for(char c : input.toCharArray()){
+        for (char c : input.toCharArray()) {
             System.out.println(c);
             int delay = 50 + rand.nextInt(100);
             try {
